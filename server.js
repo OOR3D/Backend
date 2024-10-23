78979485d6a3434f53b9be45b22dbb56bf467a08
Azure-Phoenix <larrypotter0000@gmail.com>
2024-10-23 14:19:58 -0400

    feat: init for password check

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const { password } = req.body;
+
+  const storedPassword = "password"; //don't store password in the code in production
+
+  if (password === storedPassword) {
+    res.status(200).json({ message: true });
+  } else {
+    res.status(401).json({ message: false });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
